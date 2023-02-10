7547c94f6deed57c72fb9b726f5c224b7f3bbefe
kaiwenyay <kwpwn99@gmail.com>
2023-02-10 20:43:21 +0800

    Refactor seed data functions

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,134 +1,157 @@
 import { CommunityType, PrismaClient, ChannelType } from "@prisma/client";
 const prisma = new PrismaClient();
-async function main() {
-  const alice = await prisma.user.create({
-    data : {
-      walletAddress: "Address",
-      phoneNumber: "123",
-      displayName: "alice",
-      username: "alice123",
-      email: "alice@prisma.io",
-      createdCommunities: {
-        create: {
-          name: "AliceCommunity",
-          description: "Alice's Community",
-          profilePic: "",
-          tags: ["A", "B"],
-          maxMembers: 10,
-          communityType: CommunityType.NFT,
-          channels: {
-            create: {
-              name: "Home",
-              description: "Home Channel",
-              channelType: ChannelType.REGULAR
+
+async function generateUser() {
+  await prisma.user.createMany({
+    data : [
+      {
+        walletAddress: "Address",
+        phoneNumber: "123",
+        displayName: "alice",
+        username: "alice123",
+        email: "alice@prisma.io",
+      },
+      {
+        walletAddress: "Address",
+        phoneNumber: "456",
+        displayName: "bob",
+        username: "bob456",
+        email: "bob@prisma.io"
+      }
+    ]
+  })
+}
+
+async function generateCommunity() {
+  await prisma.community.create({
+    data: {
+      name: "AliceCommunity",
+      description: "Alice's Community",
+      profilePic: "",
+      tags: ["A", "B"],
+      maxMembers: 10,
+      communityType: CommunityType.NFT,
+      creator: {
+        connect: {
+          userId: 1
+        }
+      }
+    }
+  })
+
+  await prisma.community.create({
+    data: {
+      name: "BobCommunity",
+      description: "Bob's Community",
+      profilePic: "",
+      tags: ["A", "B"],
+      maxMembers: 10,
+      communityType: CommunityType.NFT,
+      creator: {
+        connect: {
+          userId: 2
+        }
+      }
+    }
+  })
+}
+
+async function generateChannel() {
+  await prisma.channel.create({
+    data: {
+      name: "Home",
+      description: "Home Channel",
+      community: {
+        connect: {
+          communityId: 1
+        }
+      }
+    }
+  })
+
+  await prisma.channel.create({
+    data: {
+      name: "Home",
+      description: "Home Channel",
+      community: {
+        connect: {
+          communityId: 2
+        }
+      }
+    }
+  })
+}
+
+async function generatePost() {
+  await prisma.post.create({
+    data: {
+      title: "Check out Prisma with Next.js",
+        content: "https://www.prisma.io/nextjs",
+        media: ["A", "B"],
+        isPinned: false,
+        creator: {
+          connect: {
+            userId: 1
+          }
+        },
+        channel: {
+          connect: {
+            communityId_name: {
+              communityId: 1,
+              name: "Home"
             }
           }
-        }       
-      },
-      posts: {
-        create: {
-          title: "Check out Prisma with Next.js",
-          content: "https://www.prisma.io/nextjs",
-          media: ["A", "B"],
-          isPinned: false,
-          channel: {
-            connect: {
-              communityId_name: {
-                communityId: 1,
-                name: "Home"
-              }
-            }
-          },
         },
-      },
     }
-  });
-  const bob = await prisma.user.create({
+  })
+
+  await prisma.post.create({
     data: {
-      walletAddress: "Address",
-      phoneNumber: "456",
-      displayName: "bob",
-      username: "bob456",
-      email: "bob@prisma.io",
-      joinedChannels: {
-        create: {
-          channel: {
-            connect: {
-              communityId_name: {
-                communityId: 1,
-                name: "Home"
-              }
-            }
+      title: "Follow Prisma on Twitter",
+      content: "https://twitter.com/prisma",
+      media: ["A", "B"],
+      isPinned: false,
+      channel: {
+        connect: {
+          communityId_name: {
+            communityId: 2,
+            name: "Home"
           }
         }
       },
-      createdCommunities: {
-        create: {
-          name: "BobCommunity",
-          description: "Bob's Community",
-          profilePic: "",
-          tags: ["A", "B"],
-          maxMembers: 10,
-          communityType: CommunityType.NFT,
-          channels: {
-            create: {
-              name: "Home",
-              description: "Home Channel",
-              channelType: ChannelType.REGULAR
-            }
-          }
-        }       
+      creator: {
+        connect: {
+          userId: 2
+        }
+      }
+    }
+  })
+}
+
+async function generateComment() {
+  await prisma.comment.create({
+    data: {
+      content: "First",
+      commenter: {
+        connect: {
+          userId: 1
+        }
       },
-      posts: {
-        create: [
-          {
-            title: "Follow Prisma on Twitter",
-            content: "https://twitter.com/prisma",
-            media: ["A", "B"],
-            isPinned: false,
-            channel: {
-              connect: {
-                communityId_name: {
-                  communityId: 2,
-                  name: "Home"
-                }
-              }
-            },
-            comments: {
-              create: {
-                content: "First",
-                commenter: {
-                  connect: {
-                    userId: 1
-                  }
-                }
-              }
-            },
-            likes: {
-              connect: {
-                userId: 1
-              }
-            }
-          },
-          {
-            title: "Follow Nexus on Twitter",
-            content: "https://twitter.com/nexusgql",
-            media: ["A", "B"],
-            isPinned: false,
-            channel: {
-              connect: {
-                communityId_name: {
-                  communityId: 2,
-                  name: "Home"
-                }
-              }
-            }
-          },
-        ],
-      },
-    },
-  });
-  console.log({ alice, bob });
+      post: {
+        connect: {
+          postId: 2
+        }
+      }
+    }
+  })
+}
+
+async function main() {
+  await generateUser();
+  await generateCommunity();
+  await generateChannel();
+  await generatePost();
+  await generateComment();
 }
 main()
   .then(async () => {
