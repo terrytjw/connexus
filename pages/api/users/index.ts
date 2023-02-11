e8e748a96d184a5a28076d521d30b105dcd52117
Colin <colinchanwh@gmail.com>
2023-02-11 10:38:36 +0800

    User endpoints - username

---
+++ pages/api/users/index.ts
@@ -0,0 +1,77 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { handleError, ErrorResponse } from "../../../lib/prisma-util";
+import { PrismaClient, User } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     description: Returns a list of User objects
+ *     responses:
+ *       200:
+ *         description: A list of User objects
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/User"
+ *   post:
+ *     description: Create a User object
+ *     parameters:
+ *       - in: object
+ *         name: User
+ *         required: true
+ *         description: User object to create
+ *         application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/User"
+ *     responses:
+ *       200:
+ *         description: The created User object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/User"
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<User[] | ErrorResponse>
+) {
+  const { method } = req;
+
+  switch (req.method) {
+    case "GET":
+      await handleGET();
+      break;
+    case "POST":
+      const user = JSON.parse(JSON.stringify(req.body)) as Event;
+      await handlePOST(user);
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  async function handleGET() {
+    try {
+      const users = await prisma.user.findMany();
+      res.status(200).json(users);
+    } catch (error) {
+      const errorResponse = handleError(error);
+      res.status(400).json(errorResponse);
+    }
+  }
+
+  async function handlePOST(user: User) {
+    try {
+      const response = await prisma.user.create({
+        data: { ...user, userId: undefined },
+      });
+      res.status(200).json([response]);
+    } catch (error) {
+      const errorResponse = handleError(error);
+      res.status(400).json(errorResponse);
+    }
+  }
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,6 +10,8 @@ import {
 const prisma = new PrismaClient();
 async function main() {
   await generateEvent();
+  await generateUser(); 
+  await generateCollection(); 
 }
 main()
   .then(async () => {
@@ -21,6 +23,163 @@ main()
     process.exit(1);
   });
 
+async function generateUser(){
+
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+      username: "Alice", 
+      walletAddress : "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5",
+      displayName : "Alice", 
+      notificationBySMS : false, 
+      notificationByEmail : false,
+      twitterURL : "cooltwitterurl.com", 
+      profilePic: "https://aliceinwonderland.fandom.com/wiki/Alice",
+      bannerPic: "https://aliceinwonderland.fandom.com/wiki/Alice",
+      posts: {
+        create: {
+          title: "Check out Prisma with Next.js",
+          content: "https://www.prisma.io/nextjs",
+          published: true,
+        },
+      },
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+      username: "Bob", 
+      walletAddress : "0x91234590DD7278Aa3Ddd389Cc1E1d165CC4BAfe5",
+      displayName : "Bob", 
+      notificationBySMS : false, 
+      notificationByEmail : false,
+      twitterURL : "bobbytwittterURL.com",
+      profilePic: "https://aliceinwonderland.fandom.com/wiki/Alice",
+      bannerPic: "https://aliceinwonderland.fandom.com/wiki/Alice",
+      posts: {
+        create: [
+          {
+            title: "Follow Prisma on Twitter",
+            content: "https://twitter.com/prisma",
+            published: true,
+          },
+          {
+            title: "Follow Nexus on Twitter",
+            content: "https://twitter.com/nexusgql",
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+}
+
+async function generateCollection(){
+
+  const collection1 = await prisma.collection.upsert({ 
+    where: {description : "very cool collection",},
+    update: {}, 
+    create:{
+      description : "very cool collection",
+      fixedPrice : 20.0,
+      currency : "USD", 
+      collectionState : "CREATED", 
+      collections:{
+        create: {
+          media: "....com" , 
+          description: "cool items", 
+          numOfMerch : 200, 
+        }
+      }
+    }
+
+  });
+
+  const collection2 = await prisma.collection.upsert({ 
+    where: {description : "awesome collections",},
+    update: {}, 
+    create:{
+      description : "awesome collections",
+      fixedPrice : 2.0,
+      currency : "BTC", 
+      collectionState : "CREATED", 
+      collections:{
+        create: {
+          media: "....com" , 
+          description: "cool items", 
+          numOfMerch : 20, 
+        }
+      }
+    }
+
+  });
+
+
+  const collection3 = await prisma.collection.upsert({ 
+    where: {description : "porsche nfts",},
+    update: {}, 
+    create:{
+      description : "porsche nfts",
+      fixedPrice : 20.0,
+      currency : "ETH", 
+      collectionState : "CREATED", 
+      collections:{
+        create: {
+          media: "....com" , 
+          description: "cool items", 
+          numOfMerch : 20, 
+        }
+      }
+    }
+
+  });
+
+
+  const collection4 = await prisma.collection.upsert({ 
+    where: {description : "Nike NFT",},
+    update: {}, 
+    create:{
+      description : "NIKE NFT",
+      fixedPrice : 20.0,
+      currency : "USD", 
+      collectionState : "CREATED", 
+      collections:{
+        create: {
+          media: "....com" , 
+          description: "cool items", 
+          numOfMerch : 2020, 
+        }
+      }
+    }
+
+  });
+
+
+  const collection5 = await prisma.collection.upsert({ 
+    where: {description : "Addidas",},
+    update: {}, 
+    create:{
+      description : "Addidas",
+      fixedPrice : 1.0,
+      currency : "BTC", 
+      collectionState : "CREATED", 
+      collections:{
+        create: {
+          media: "....com" , 
+          description: "cool items", 
+          numOfMerch : 2500, 
+        }
+      }
+    }
+
+  });
+}
 async function generateEvent() {
   await prisma.event.create({
     data: {
