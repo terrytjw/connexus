29838a40a050ab03571b6a3ec8284dbd953406db
laiwei <laiweileeee@gmail.com>
2023-02-13 00:04:32 +0800

    feat: view events page now conditionally renders creator an fan view based on a toggle state

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Toggle from "../components/Toggle";
+import CreatorEventsPage from "../components/EventPages/creatorEvents";
+import FanEventsPage from "../components/EventPages/fanEvents";
 
 const EventsPage = () => {
-  return <div>EventsPage</div>;
+  const [isCreator, setIsCreator] = useState<boolean>(false);
+  return (
+    <div>
+      <div className="flex justify-start p-8">
+        {isCreator ? "Creator" : "Fan"}
+        <Toggle isChecked={isCreator} setIsChecked={setIsCreator} />
+      </div>
+      {isCreator ? <CreatorEventsPage /> : <FanEventsPage />}
+    </div>
+  );
 };
 
 export default EventsPage;
