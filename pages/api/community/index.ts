2f89dd609921f9ae297f73d8291eed9e86e31ebe
kaiwenyay <kwpwn99@gmail.com>
2023-02-12 23:27:04 +0800

    Added CRUD for community

---
+++ pages/api/community/index.ts
@@ -0,0 +1,115 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { handleError, ErrorResponse } from "../../../lib/prisma-util";
+import { PrismaClient, Community } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+/**
+ * @swagger
+ * /api/community:
+ *   get:
+ *     description: Returns a list of Community objects
+ *     responses:
+ *       200:
+ *         description: A list of Community objects
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Community"
+ *   post:
+ *     description: Create a Community object
+ *     parameters:
+ *       - in: object
+ *         name: Community
+ *         required: true
+ *         description: Community object to create
+ *         application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Community"
+ *     responses:
+ *       200:
+ *         description: The created Community object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Community"
+ *   put:
+ *     description: Update a Community object
+ *     parameters:
+ *       - in: object
+ *         name: Community
+ *         required: true
+ *         description: Community object to update
+ *         application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Community"
+ *     responses:
+ *       200:
+ *         description: The updated Community object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Community"
+ *   delete:
+ *     description: Delete a Community object
+ *     parameters:
+ *       - in: object
+ *         name: Community
+ *         required: true
+ *         description: Community object to delete
+ *         application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Community"
+ *     responses:
+ *       200:
+ *         description: The deleted Community object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Community"
+ */
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Community[] | ErrorResponse>
+) {
+  const { method, body } = req;
+
+  const community = JSON.parse(JSON.stringify(body)) as Community;
+
+  switch (method) {
+    case "GET":
+      await handleGET();
+      break;
+    case "POST":
+      await handlePOST(community);
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+
+  async function handleGET() {
+    try {
+      const communities = await prisma.community.findMany({
+        
+      });
+      res.status(200).json(communities);
+    } catch (error) {
+      const errorResponse = handleError(error);
+      res.status(400).json(errorResponse);
+    }
+  }
+
+  async function handlePOST(community: Community) {
+    try {
+      const response = await prisma.community.create({
+        data: { ...community }
+      });
+      res.status(200).json([response]);
+    } catch (error) {
+      const errorResponse = handleError(error);
+      res.status(400).json(errorResponse);
+    }
+  }
+}
