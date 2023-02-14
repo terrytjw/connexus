36a807113d6db8010c99d8cf0592837823dd42a3
shengliang <ahliang51@hotmail.com>
2023-02-15 00:14:10 +0800

    incorporated next auth with jwt and session

--- pages/chats.tsx
+++ pages/chats.tsx
@@ -1,7 +1,22 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 import React from "react";
 
-const ChatsPage = () => {
+const ChatsPage = ({}) => {
   return <div>ChatsPage</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  console.log(session);
+  // session?.user.
+  return {
+    props: {
+      user: {
+        // id : session?.walletAddress,
+      },
+    },
+  };
 };
 
 export default ChatsPage;
--- pages/events.tsx
+++ pages/events.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { getServerSession } from "next-auth/next";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 
 const EventsPage = () => {
   return <div>EventsPage</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  console.log(session);
+  return {
+    props: {},
+  };
 };
 
 export default EventsPage;
