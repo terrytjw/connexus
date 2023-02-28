b4c5f352e07d1dba62da4e96ecd06e4d34cac806
niclqt <nicoletan2401@gmail.com>
2023-02-28 19:52:48 +0800

    integrate channel, post, comment api

--- components/CommunityPages/Comment.tsx
+++ components/CommunityPages/Comment.tsx
@@ -1,48 +1,96 @@
+import axios from "axios";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { FaRegHeart, FaTrashAlt } from "react-icons/fa";
+import { FaHeart, FaRegHeart, FaTrashAlt } from "react-icons/fa";
 import Button from "../Button";
 import CustomLink from "../CustomLink";
-import { Comment } from "../../utils/types";
+import { CommentWithCommenterAndLikes } from "../../utils/types";
 
 type CommentProps = {
-  comment: Comment;
-  replyTo: Function;
+  comment: CommentWithCommenterAndLikes;
+  replyTo: () => void;
+  mutateComments: any;
 };
 
-const Comment = ({ comment, replyTo }: CommentProps) => {
+const Comment = ({ comment, replyTo, mutateComments }: CommentProps) => {
+  const { data: session } = useSession();
+  const userId = Number(session?.user.userId);
+
+  const likeComment = async () => {
+    const res = await axios.post(
+      `http://localhost:3000/api/comment/${comment.commentId}/like?userId=${userId}`
+    );
+
+    const temp = res.data;
+    mutateComments((data: CommentWithCommenterAndLikes[]) => {
+      data
+        .find((comment) => comment.commentId == temp.commentId)
+        ?.likes.push({ userId: userId });
+
+      return data;
+    });
+  };
+
+  const unlikeComment = async () => {
+    const res = await axios.post(
+      `http://localhost:3000/api/comment/${comment.commentId}/unlike?userId=${userId}`
+    );
+
+    const temp = res.data;
+    mutateComments((data: CommentWithCommenterAndLikes[]) => {
+      data
+        .find((comment) => comment.commentId == temp.commentId)
+        ?.likes.filter((like) => like.userId != userId);
+
+      return data;
+    });
+  };
+
+  const deleteComment = async () => {
+    const res = await axios.delete(
+      `http://localhost:3000/api/comment/${comment.commentId}`
+    );
+
+    const temp = res.data;
+    mutateComments((data: CommentWithCommenterAndLikes[]) => {
+      return data.filter((comment) => comment.commentId != temp.commentId);
+    });
+  };
+
   return (
     <div className="flex gap-4 px-8 py-4">
       <div>
         <Image
           height={48}
           width={48}
-          className="rounded-full"
-          src={comment.commentor.profilePic}
+          className="aspect-square rounded-full object-cover object-center"
+          src={comment.commenter.profilePic ?? ""}
           alt="Current user profile pic"
         />
       </div>
       <div className="flex-grow">
         <div className="flex w-full flex-col gap-1">
           <CustomLink
-            href={`/user/profile/${comment.commentor.userId}`}
+            href={`/user/profile/${comment.commenter.userId}`}
             className="text-gray-700"
           >
-            {comment.commentor.displayName}
+            {comment.commenter.username}
           </CustomLink>
           <p className="break-all text-sm">{comment.content}</p>
         </div>
 
         <div className="mt-2 flex items-center gap-4">
-          {comment.likes > 0 ? (
+          {comment.likes.length > 0 ? (
             <span className="text-sm">
-              {comment.likes} like{comment.likes != 1 ? "s" : null}
+              {comment.likes.length} like
+              {comment.likes.length != 1 ? "s" : null}
             </span>
           ) : null}
-          <button className="text-sm hover:underline" onClick={() => replyTo()}>
+          {/* <button className="text-sm hover:underline" onClick={() => replyTo()}>
             Reply
-          </button>
+          </button> */}
           <span className="text-sm text-gray-500">
-            {comment.date.toLocaleString("en-gb", {
+            {new Date(comment.date).toLocaleString("en-gb", {
               day: "numeric",
               month: "short",
               year: "numeric",
@@ -50,20 +98,38 @@ const Comment = ({ comment, replyTo }: CommentProps) => {
           </span>
         </div>
       </div>
-      <Button
-        size="sm"
-        variant="outlined"
-        className="!btn-circle self-center border-0"
-      >
-        <FaRegHeart />
-      </Button>
-      <Button
-        size="sm"
-        variant="outlined"
-        className="!btn-circle self-center border-0"
-      >
-        <FaTrashAlt />
-      </Button>
+      {comment.likes.find(
+        (like: { userId: number }) => like.userId == userId
+      ) ? (
+        <Button
+          size="sm"
+          variant="outlined"
+          className="!btn-circle self-center border-0"
+          onClick={() => unlikeComment()}
+        >
+          <FaHeart />
+        </Button>
+      ) : (
+        <Button
+          size="sm"
+          variant="outlined"
+          className="!btn-circle self-center border-0"
+          onClick={() => likeComment()}
+        >
+          <FaRegHeart />
+        </Button>
+      )}
+
+      {comment.commenter.userId == userId ? (
+        <Button
+          size="sm"
+          variant="outlined"
+          className="!btn-circle self-center border-0"
+          onClick={() => deleteComment()}
+        >
+          <FaTrashAlt />
+        </Button>
+      ) : null}
     </div>
   );
 };
--- pages/communities/[id].tsx
+++ pages/communities/[id].tsx
@@ -1,25 +1,60 @@
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
-import Toggle from "../../components/Toggle";
 import CreatorCommunityPage from "../../components/CommunityPages/Creator/CreatorCommunityPage";
 import FanCommunityPage from "../../components/CommunityPages/Fan/FanCommunityPage";
-import ProtectedRoute from "../../components/ProtectedRoute";
 import Layout from "../../components/Layout";
+import ProtectedRoute from "../../components/ProtectedRoute";
+import { CommunityWithCreatorAndChannelsAndMembers } from "../../utils/types";
 
-const CommunityPage = () => {
-  const [isCreator, setIsCreator] = useState(true);
+type CommunityPagePageProps = {
+  communityData: CommunityWithCreatorAndChannelsAndMembers;
+};
+
+const CommunityPage = ({ communityData }: CommunityPagePageProps) => {
+  const { data: session } = useSession();
+  const userId = Number(session?.user.userId);
+
+  const [community, setCommunity] = useState(communityData);
 
   return (
     <ProtectedRoute>
       <Layout>
-        {/* to be removed after pr approved */}
-        <div className="mt-4 flex gap-2">
-          Toggle creator / fan view
-          <Toggle isChecked={isCreator} setIsChecked={setIsCreator} />
-        </div>
-        {isCreator ? <CreatorCommunityPage /> : <FanCommunityPage />}
+        {community.userId == userId ? (
+          <CreatorCommunityPage
+            community={community}
+            setCommunity={setCommunity}
+          />
+        ) : (
+          <FanCommunityPage community={community} setCommunity={setCommunity} />
+        )}
       </Layout>
     </ProtectedRoute>
   );
 };
 
 export default CommunityPage;
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  const { id } = context.params;
+
+  const { data: communityData } = await axios.get(
+    `http://localhost:3000/api/community/${id}`
+  );
+
+  if (communityData && Object.keys(communityData).length === 0) {
+    return {
+      redirect: {
+        destination: "/communities",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      communityData,
+    },
+  };
+};
