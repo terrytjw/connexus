3f82ca9ffa9eb981ca910e6a029c5f4897a3dbb1
shengliang <ahliang51@hotmail.com>
2023-03-06 20:16:40 +0800

    WIP for rewrite

---
+++ lib/api-helpers/event-api.ts
@@ -0,0 +1,28 @@
+import { EVENT_ENDPOINT } from "./../constant";
+import { Collection, User } from "@prisma/client";
+import { API_URL, COLLECTION_ENDPOINT, USER_ENDPOINT } from "../constant";
+import axios from "axios";
+
+export async function getEventInfo(eventId: number) {
+  const url = `${API_URL}/${EVENT_ENDPOINT}/${eventId}`;
+  const response = (await axios.get(url)).data;
+  return response;
+}
+
+export async function updateEventInfo(eventId: number, eventInfo: Event) {
+  const url = `${API_URL}/${EVENT_ENDPOINT}/${eventId}`;
+  const response = (await axios.post(url, eventInfo)).data;
+  return response;
+}
+
+export async function createEvent(eventInfo: Event) {
+  const url = `${API_URL}/${EVENT_ENDPOINT}`;
+  const response = (await axios.post(url, eventInfo)).data;
+  return response;
+}
+
+export async function registerEvent(eventId: number) {
+  const url = `${API_URL}/${EVENT_ENDPOINT}/${eventId}/register`;
+  const response = (await axios.post(url)).data;
+  return response;
+}
