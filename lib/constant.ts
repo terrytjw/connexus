366b8f3fb00ba4fb1e22be1fd5a7c84033beba4b
shengliang <ahliang51@hotmail.com>
2023-03-15 00:39:55 +0800

    re-added buckets

--- lib/constant.ts
+++ lib/constant.ts
@@ -9,6 +9,8 @@ export const COMMUNITY_ENDPOINT = "community";
 export const USER_PROFILE_BUCKET = "user-profile";
 export const EVENT_PROFILE_BUCKET = "event-profile";
 export const MERCH_PROFILE_BUCKET = "merch-profile";
+export const COMMUNITY_BUCKET = "community";
+export const POST_BUCKET = "post";
 
 export const smartContract = {
   pinataApiKey: "be74f69d81d8435228e2",
