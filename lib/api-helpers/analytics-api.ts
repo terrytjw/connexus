143b7944574b7a5306866b559e52dcc227f3fb23
kaiwenyay <kwpwn99@gmail.com>
2023-03-19 19:33:51 +0800

    Analytics api helpers

--- lib/api-helpers/analytics-api.ts
+++ lib/api-helpers/analytics-api.ts
@@ -5,14 +5,111 @@ import { lastWeek } from "../../utils/date-util";
 
 const baseUrl = `${API_URL}/${ANALYTICS_ENDPOINT}`;
 
-export async function getChannelAnalyticsInRangeAPI(
+export async function getChannelAnalyticsByCreatorAPI(
   userId: number,
   lowerBound: Date = lastWeek(),
   upperBound: Date = new Date()
 ) {
-  const response = (await axios.get(baseUrl, {
+  const url = baseUrl + '/channel'
+  const response = (await axios.get(url, {
     params: {
       userId: userId,
+      lowerBound: lowerBound,
+      upperBound: upperBound
+    }
+  })).data;
+  return response;
+}
+
+export async function getChannelAnalyticsByChannelAPI(
+  channelId: number,
+  lowerBound: Date = lastWeek(),
+  upperBound: Date = new Date()
+) {
+  const url = baseUrl + '/channel/filter'
+  const response = (await axios.get(url, {
+    params: {
+      channelId: channelId,
+      lowerBound: lowerBound,
+      upperBound: upperBound
+    }
+  })).data;
+  return response;
+}
+
+export async function getCommunityAnalyticsByCreatorAPI(
+  userId: number,
+  lowerBound: Date = lastWeek(),
+  upperBound: Date = new Date()
+) {
+  const url = baseUrl + '/community'
+  const response = (await axios.get(url, {
+    params: {
+      userId: userId,
+      lowerBound: lowerBound,
+      upperBound: upperBound
+    }
+  })).data;
+  return response;
+}
+
+export async function getCollectionAnalyticsByCreatorAPI(
+  userId: number,
+  lowerBound: Date = lastWeek(),
+  upperBound: Date = new Date()
+) {
+  const url = baseUrl + '/collections'
+  const response = (await axios.get(url, {
+    params: {
+      userId: userId,
+      lowerBound: lowerBound,
+      upperBound: upperBound
+    }
+  })).data;
+  return response;
+}
+
+export async function getCollectionAnalyticsByCollectionAPI(
+  channelId: number,
+  lowerBound: Date = lastWeek(),
+  upperBound: Date = new Date()
+) {
+  const url = baseUrl + '/collection/filter'
+  const response = (await axios.get(url, {
+    params: {
+      channelId: channelId,
+      lowerBound: lowerBound,
+      upperBound: upperBound
+    }
+  })).data;
+  return response;
+}
+
+export async function getEventAnalyticsByCreatorAPI(
+  userId: number,
+  lowerBound: Date = lastWeek(),
+  upperBound: Date = new Date()
+) {
+  const url = baseUrl + '/channel'
+  const response = (await axios.get(url, {
+    params: {
+      userId: userId,
+      lowerBound: lowerBound,
+      upperBound: upperBound
+    }
+  })).data;
+  return response;
+}
+
+export async function getEventAnalyticsByEventAPI(
+  channelId: number,
+  lowerBound: Date = lastWeek(),
+  upperBound: Date = new Date()
+) {
+  const url = baseUrl + '/event/filter'
+  const response = (await axios.get(url, {
+    params: {
+      channelId: channelId,
       lowerBound: lowerBound,
       upperBound: upperBound
     }
