a17f605e871bef9a6e96d204287d7c6cb201fad9
Terry Tan <tanjunweiterry@gmail.com>
2023-04-14 16:48:37 +0800

    remove screen debugger

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,5 +38,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui"), require("tailwindcss-debug-screens")],
+  plugins: [require("daisyui") /* require("tailwindcss-debug-screens") */],
 };
